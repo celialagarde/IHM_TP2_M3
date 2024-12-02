8706bd436df555d66607d69607bcb266d60b72dd
Lagarde céliaégit config --list git config --global user.name Lagarde <celia.lagarde@lp2i-poitiers.fr>
2024-12-02 16:35:25 +0100

    @mail

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ class ConnexionApp(QMainWindow, Ui_Form):
         # Connecter le bouton Valider à une fonction
         self.bouton_valider.clicked.connect(self.valider_formulaire)
 
+    def verifier_format_email(self, email):
+        regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        return re.match(regex, email) is not None
+
+
+
 
     def valider_formulaire(self):
         # Récupérer les valeurs des champs de saisie
@@ -31,6 +37,16 @@ class ConnexionApp(QMainWindow, Ui_Form):
             self.afficher_message(resultat_analyse)
             return
 
+    # Vérifier si les champs sont correctement remplis
+        if not identifiant or not mot_de_passe or not email:
+          self.afficher_message("Erreur : veuillez remplir correctement tous les champs.")
+          return "Erreur : veuillez remplir correctement tous les champs."
+
+
+    # Vérifier si l'adresse e-mail est bien formatée
+        if not self.verifier_format_email(email):
+            self.afficher_message("Erreur : l'adresse e-mail n'est pas valide.")
+            return "Erreur : l'adresse e-mail n'est pas valide."
 
         # Si toutes les vérifications passent
         self.afficher_message("Connexion réussie.")
@@ -39,11 +55,12 @@ class ConnexionApp(QMainWindow, Ui_Form):
     def analyser_mot_de_passe(self, mot_de_passe):
         # Vérifier la longueur du mot de passe
         if len(mot_de_passe) < 8:
-             self.afficher_message("Erreur : Mot de passe trop court")
-             return "Erreur : Mot de passe trop court"
+            self.afficher_message("Erreur : Mot de passe trop court")
+            return "Erreur : Mot de passe trop court"
         else :
-             self.afficher_message("Le mot de passe est robuste.")
-             return "Le mot de passe est robuste."
+           self.afficher_message("Le mot de passe est robuste.")
+           return "Le mot de passe est robuste."
+
 
     # Fonction pour afficher un message dans le QLabel
     def afficher_message(self, message):
